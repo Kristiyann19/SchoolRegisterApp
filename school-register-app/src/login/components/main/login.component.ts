70f9c155053b107f209dbd4fc405e36517c297ca
DESKTOP-HHPDH9M\Ivana.Boyukliyska <iva1100b@gmail.com>
2024-03-08 14:10:49 +0200

    Client: add OnSubmit in login

--- school-register-app/src/login/components/main/login.component.ts
+++ school-register-app/src/login/components/main/login.component.ts
@@ -1,8 +1,25 @@
 import { Component } from "@angular/core";
+import { LoginService } from "../../services/login.service";
+import { Router } from "@angular/router";
+import { UserLoginDto } from "../../../user/dtos/user-login-dto";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrl: "./login.component.css",
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginDto: UserLoginDto = new UserLoginDto();
+
+  constructor(private loginService: LoginService, private router: Router) {}
+
+  onSubmit() {
+    this.loginService.login(this.loginDto).subscribe((response: any) => {
+      if (response?.token) {
+        localStorage.setItem("token", response.token);
+        this.loginService.initializeUser();
+        this.router.navigate(["/"]);
+      }
+    });
+  }
+}
