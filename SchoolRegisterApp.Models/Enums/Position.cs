80d658453653aadb839ddbbd11455574435b0c6c
DESKTOP-HHPDH9M\Ivana.Boyukliyska <iva1100b@gmail.com>
2024-03-08 10:27:14 +0200

    Server: add models and enums

---
+++ SchoolRegisterApp.Models/Enums/Position.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolRegisterApp.Models.Enums
+{
+    public enum Position
+    {
+        Mathematics = 1,
+        Geography = 2,
+        History = 3,
+        Biology = 4,
+        Chemistry = 5,
+        Literature = 6,
+        English = 7
+    }
+}
