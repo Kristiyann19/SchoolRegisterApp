2e86adaa397da1cab332e0513ccb9ca630082801
DESKTOP-14PH1HC\Kristiyan.Karamilev <karamilevkristiyan@gmail.com>
2024-03-08 14:03:06 +0200

    client side register and login html

--- school-register-app/src/registration/components/registration.component.ts
+++ school-register-app/src/registration/components/registration.component.ts
@@ -1,8 +1,29 @@
 import { Component } from "@angular/core";
+import { UserRegistrationDto } from "../../user/dtos/user-registration-dto";
+import { Router } from "@angular/router";
+import { RegistrationService } from "../services/registration.service";
 
 @Component({
   selector: "app-registration",
   templateUrl: "./registration.component.html",
   styleUrl: "./registration.component.css",
 })
-export class RegistrationComponent {}
+export class RegistrationComponent {
+
+  //schools: SchoolDto[] = [];
+  
+  register: UserRegistrationDto = new UserRegistrationDto();
+  
+  constructor(private registerService: RegistrationService, private router: Router, ) { }
+  onRegister() : void {
+    this.registerService.register(this.register).subscribe(
+      () => {
+        console.log('User registered successfuly');
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        console.error('Register failed.', error)
+      }
+    )
+  }
+}
--- school-register-app/src/registration/services/registration.service.ts
+++ school-register-app/src/registration/services/registration.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { Router } from "@angular/router";
 import { UserRegistrationDto } from "../../user/dtos/user-registration-dto";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: "root",
