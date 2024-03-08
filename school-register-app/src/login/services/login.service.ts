19238ad138b2ebb710b4805732d1b2f39d06c38e
DESKTOP-HHPDH9M\Ivana.Boyukliyska <iva1100b@gmail.com>
2024-03-08 13:43:44 +0200

    Client: add login, register, nav

---
+++ school-register-app/src/login/services/login.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { UserDto } from "../../user/dtos/user-dto";
+import { UserRegistrationDto } from "../../user/dtos/user-registration-dto";
+import { UserLoginDto } from "../../user/dtos/user-login-dto";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LoginService {
+  private baseUrl = "http://localhost:12123";
+  public currentUser: UserDto = new UserDto();
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  initializeUser(): Promise<{}> {
+    return new Promise((resolve) => {
+      return this.http
+        .get<UserDto>("http://localhost:12123/api/Users/CurrentUser")
+        .subscribe((userData) => {
+          this.currentUser = userData;
+          resolve(true);
+        });
+    });
+  }
+
+  register(userDto: UserRegistrationDto): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/api/Register`, userDto);
+  }
+
+  login(userDto: UserLoginDto): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/api/Login`, userDto);
+  }
+
+  logout(): Observable<void> {
+    this.currentUser = null;
+    localStorage.clear();
+    this.router.navigate(["/"]);
+    return;
+  }
+}
