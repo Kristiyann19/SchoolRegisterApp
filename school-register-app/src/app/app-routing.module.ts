3ef2c96355fb9a18595e9c99a3af9ab19bc32244
DESKTOP-14PH1HC\Kristiyan.Karamilev <karamilevkristiyan@gmail.com>
2024-03-13 13:55:50 +0200

    person school tab

--- school-register-app/src/app/app-routing.module.ts
+++ school-register-app/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { DetailsPersonComponent } from "../people/components/details-person/deta
 
 import { AddPersonComponent } from "../people/components/add-person/add-person.component";
 import { PersonHistoryComponent } from "../person-history/components/person-history.component";
+import { PersonSchoolComponent } from "../person-school/components/person-school.component";
 
 
 const routes: Routes = [
@@ -20,7 +21,8 @@ const routes: Routes = [
   { path: "all-people", component: AllPeopleComponent },
   { path: "person/:id", component: DetailsPersonComponent},
   { path: "add-person", component: AddPersonComponent },
-  { path: "person-history/:id", component: PersonHistoryComponent}
+  { path: "person-history/:id", component: PersonHistoryComponent},
+  { path: "person-school/:id", component: PersonSchoolComponent}
 
 ];
 
