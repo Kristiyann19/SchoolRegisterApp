cde7dc387bfb1d2edf45663129f2b1100d0e4c7a
DESKTOP-14PH1HC\Kristiyan.Karamilev <karamilevkristiyan@gmail.com>
2024-03-13 13:36:06 +0200

    ..

--- school-register-app/src/person-school/components/person-school.component.ts
+++ school-register-app/src/person-school/components/person-school.component.ts
@@ -4,6 +4,8 @@ import { PersonSchoolDto } from "../dtos/person-school.dto";
 import { PositionEnumLocalization } from "../../enums/position.enum";
 import { PersonSchoolService } from "../service/person-school.service";
 import { SettlementService } from "../../settlement/services/settlement.service";
+import { catchError, throwError } from "rxjs";
+import { SettlementDto } from "../../settlement/dtos/settlement-dto";
 
 @Component({
   selector: "app-person-school",
@@ -13,6 +15,7 @@ import { SettlementService } from "../../settlement/services/settlement.service"
 export class PersonSchoolComponent {
   personSchools: PersonSchoolDto[] = [];
   positionEnumLocalization = PositionEnumLocalization;
+  settlements: SettlementDto[] = [];
 
   constructor (private personSchoolService: PersonSchoolService, private route: ActivatedRoute, private settlementService: SettlementService) {}
 
@@ -20,19 +23,20 @@ export class PersonSchoolComponent {
     const id = parseInt(this.route.snapshot.paramMap.get('id')!)
     this.personSchoolService.getPersonSchoolById(id).subscribe((personSchools: PersonSchoolDto[]) => {
       this.personSchools = personSchools;
+      this.getSettlements();
     })
   } 
 
-  // getSettlements() {
-  //   this.settlementService
-  //     .getAll()
-  //     .pipe(
-  //       catchError((err) => {
-  //         return throwError(() => err);
-  //       })
-  //     )
-  //     .subscribe((res) => {
-  //       this.settlements = res;
-  //     });
-  // }
+  getSettlements() {
+    this.settlementService
+      .getAll()
+      .pipe(
+        catchError((err) => {
+          return throwError(() => err);
+        })
+      )
+      .subscribe((res) => {
+        this.settlements = res;
+      });
+  }
 }
