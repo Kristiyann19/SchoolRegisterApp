c5328ac75102eac74e0a060e01f4f43c28a774f2
DESKTOP-HHPDH9M\Ivana.Boyukliyska <iva1100b@gmail.com>
2024-03-15 09:57:29 +0200

    Server: change EndDate in PersonSchoolDto to be nullable

--- SchoolRegisterApp.Models/Dtos/PersonSchoolDto.cs
+++ SchoolRegisterApp.Models/Dtos/PersonSchoolDto.cs
@@ -14,6 +14,6 @@ namespace SchoolRegisterApp.Models.Dtos
 
         public DateTime StartDate { get; set; }
 
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
     }
 }
